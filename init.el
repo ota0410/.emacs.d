175cc8f434c34a9a39a03e2c202c4ebb01776aa9
xyz <s1411506@klis.tsukuba.ac.jp>
2020-12-10 20:30:36 +0900

    Do not create bak

--- init.el
+++ init.el
@@ -17,9 +17,9 @@
 ;;add load path "elisp" "conf"
 (add-to-load-path "elisp")
 
-<<<<<<< HEAD
-;;
-=======
+;;do not create bakeup file
+(setq make-backup-files t)
+
 ;;swap-buffer
 (defun swap-buffers ()
   "Swapping buffers in two windows"
@@ -39,7 +39,7 @@
 (global-set-key [f2] 'swap-buffers-keep-focus)
 (global-set-key [C-f2] 'swap-buffers)
 
->>>>>>> origin/master
+
 ;;use-packageをrequire の代わりに使う
 (if (require 'use-package nil t)
     (setq use-package-verbose t)
@@ -58,30 +58,11 @@
 (define-key global-map (kbd "C-t") 'beginning-of-buffer)
 
 
-;;completion
-(require 'company)
-(global-company-mode) ; 全バッファで有効にする
-(setq company-auto-expand t) ;; 1個目を自動的に補完
-(setq company-transformers '(company-sort-by-backend-importance)) ;; ソート順
-(setq company-idle-delay 0) ; 遅延なしにすぐ表示
-(setq company-minimum-prefix-length 2) ; デフォルトは4
-(setq company-selection-wrap-around t) ; 候補の最後の次は先頭に戻る
-(setq completion-ignore-case t)
-(setq company-dabbrev-downcase nil)
-(global-set-key (kbd "M-S-i") 'company-complete)
-
-;; C-n, C-pで補完候補を次/前の候補を選択
-(define-key company-active-map (kbd "C-n") 'company-select-next)
-(define-key company-active-map (kbd "C-p") 'company-select-previous)
-(define-key company-active-map [tab] 'company-complete-selection) ;; TABで候補を設定
-(define-key company-active-map (kbd "C-h") nil) ;; C-hはバックスペース割当のため無効化
-(define-key company-active-map (kbd "C-S-h") 'company-show-doc-buffer) ;; ドキュメント表示はC-S
-  
 ;;load modes
 (load "init-global") ;;共通の設定を記述
 (load "markdown-mode")
 (load "matlab")
-(load "php-mode")
-;;(load "cuda-mode")
+;;(load "php-mode")
+(load "cuda-mode")
 ;;(load "init-utility")
 (put 'downcase-region 'disabled nil)
