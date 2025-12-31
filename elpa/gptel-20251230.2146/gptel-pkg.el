b8707c1dd0a4355e2c59e1b874f944a9275b2cef
ro644 <s1411506@klis.tsukuba.ac.jp>
2026-01-01 01:06:15 +0900

    Add dir elpa

---
+++ elpa/gptel-20251230.2146/gptel-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel" "20251230.2146"
+  "Interact with ChatGPT or other LLMs."
+  '((emacs     "27.1")
+    (transient "0.7.4")
+    (compat    "30.1.0.0"))
+  :url "https://github.com/karthink/gptel"
+  :commit "850c20689fe00e18b13047c784c77f0df8ab6ca9"
+  :revdesc "850c20689fe0"
+  :keywords '("convenience" "tools")
+  :authors '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")))
